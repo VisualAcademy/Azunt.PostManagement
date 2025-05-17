afbf74aaaa0d73e7c1d354233a4ad439606b726f
Park YongJune <redplus@redplus.net>
2025-05-18 06:17:05 +0900

    Add IPostStorageService interface

---
+++ src/Azunt.PostManagement/Azunt.PostManagement/02_Contracts/IPostStorageService.cs
@@ -0,0 +1,9 @@
+﻿namespace Azunt.PostManagement
+{
+    public interface IPostStorageService
+    {
+        Task<string> UploadAsync(Stream postStream, string postName);
+        Task<Stream> DownloadAsync(string postName);
+        Task DeleteAsync(string postName);
+    }
+}
